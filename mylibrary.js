fedbdc7fa3877769b79dc09afcec56ae9c21aa72
maliakos <maliakos@hotmail.com>
2022-01-03 02:27:47 +0200

    Needs polishing especially the UI.Functionality is fine ,needs more testing.Probably will be used to add features on later lessons.

--- mylibrary.js
+++ mylibrary.js
@@ -15,8 +15,9 @@ function Book(title, author, pageNumber, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-function createCard(obj = new Book()) {
+function createCard(obj = new Book(), index) {
   cardContainer = document.querySelector(".grid-container");
+
   card = document.createElement("div");
   card.classList.add("card");
   cardContainer.appendChild(card);
@@ -30,20 +31,44 @@ function createCard(obj = new Book()) {
   cardContent.classList.add("card-content");
   card.appendChild(cardContent);
 
-  cardContent.insertAdjacentHTML(
+  cardUnorderedList = document.createElement("ul");
+  cardContent.appendChild(cardUnorderedList);
+  cardUnorderedList.insertAdjacentHTML(
     "beforeend",
     `<li>Author: <span> ${obj.author}</span></li>`
   );
-  cardContent.insertAdjacentHTML(
+  cardUnorderedList.insertAdjacentHTML(
     "beforeend",
     `<li>Pages: <span> ${obj.pageNumber}</span></li>`
   );
-  cardContent.insertAdjacentHTML(
+  cardUnorderedList.insertAdjacentHTML(
     "beforeend",
     `<li>Read: <span> ${obj.read ? "Yes!" : "Not Yet!"}</span></li>`
   );
+  cardButtonGroup = document.createElement("div");
+  cardButtonGroup.classList.add("button-group");
+  cardContent.appendChild(cardButtonGroup);
+
+  cardButtonRead = document.createElement("button");
+  cardButtonRead.setAttribute("data-index", index);
+  cardButtonDelete = cardButtonRead.cloneNode(true);
+  cardButtonRead.classList.add(obj.read ? "read" : "unread");
+  cardButtonDelete.classList.add("delete");
+  cardButtonRead.textContent = obj.read ? "Read" : "Not Read";
+  cardButtonDelete.textContent = "Delete";
+  cardButtonGroup.appendChild(cardButtonRead);
+  cardButtonGroup.appendChild(cardButtonDelete);
 }
 
+function displayCards(arr = []) {
+  arr.forEach((card, index) => {
+    createCard(card, index);
+  });
+}
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 theHobbit = new Book("The Hobbit", "J.R. Tolkien", "800", true);
 theFly = new Book("The Fly", "Jason Statham", "600", false);
 harryPotter_1 = new Book("Harry Potter 1", "J.K Rowling", "483", true);
@@ -54,4 +79,37 @@ addBookToLibrary(theFly);
 addBookToLibrary(harryPotter_1);
 addBookToLibrary(harryPotter_2);
 addBookToLibrary(harryPotter_3);
-console.log(myLibrary);
+
+displayCards(myLibrary);
+
+//Event Listeners
+const addBookButton = document.getElementById("add_book");
+addBookButton.addEventListener("click", () => {
+  document.querySelector(".add-book-modal").style.display = "flex";
+});
+const deleteButtons = document.querySelectorAll(".delete");
+console.log(deleteButtons);
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    myLibrary.splice(e.target.dataset.index, 1);
+    console.log(myLibrary);
+    e.target.parentNode.parentNode.parentNode.remove();
+    document.querySelectorAll(".card .delete").forEach((button, index) => {
+      button.dataset.index = index;
+    });
+  });
+});
+
+const readButtons = document.querySelectorAll(
+  ".button-group button:not(.delete)"
+);
+readButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    myLibrary[+e.target.dataset.index].toggleRead();
+    e.target.classList.toggle("read");
+    e.target.classList.toggle("unread");
+    e.target.innerHTML == "Read"
+      ? (e.target.innerHTML = "Not Read")
+      : (e.target.innerHTML = "Read");
+  });
+});
